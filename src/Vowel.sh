5559a6b1f582baebcbe8f5687efe5560bc02c0a9
Sujata Dev <sujatadev97@gmail.com>
2017-05-01 01:13:48 +0530

    renamed to Vowel.sh

---
+++ src/Vowel.sh
@@ -0,0 +1,31 @@
+#Shell script to calculate no. of words, characters and spaces of a given
+#string. And find out whether given character is vowel or consonant
+echo -e "\n1)Enter string"
+echo -e "2)Enter character"
+echo -e "Enter choice: "
+read ch
+case "$ch" in
+1)	echo -e "\nEnter string: "
+	read str
+	words=`echo -n $str|wc -w`	# -n means no newline, w means 
+	chars=`echo -n $str|wc -c`
+	space=`expr $words - 1`
+	#wc -l prints the line count
+	#wc -m prints the character count
+	#wc -w prints the word count
+	echo "No. of words: $words"
+	echo "No. of characters: $chars"
+	echo "No. of spaces: $space"
+	;;
+2)	echo -e "\nEnter character: "
+	read char
+	if [ $char == "a" -o $char == "e" -o $char == "i" -o $char == "o" -o $char == "u" ]
+	then
+		echo -e "\nCharacter is a vowel"
+	else
+		echo -e "\nCharacter is a consonant"
+	fi
+	;;
+*)	echo -e "\nInvalid Choice"
+	;;
+esac
