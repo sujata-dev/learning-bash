0e48f36e302eab777868b11425d8bd6de48483f7
Sujata Dev <sujatadev97@gmail.com>
2017-05-01 01:15:18 +0530

    renamed

--- src/3Swap.sh
+++
@@ -1,7 +0,0 @@
-#Shell script to swap two numbers without using third variable
-echo -e "\nEnter 2 numbers: "
-read a b
-a=`expr $a + $b`
-b=`expr $a - $b`
-a=`expr $a - $b`
-echo -e "\nSwapped numbers: $a $b"
